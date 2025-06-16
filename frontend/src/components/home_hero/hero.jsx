ff5139aa168dd38bc6342bc8d80e768472692dd0
Muhammad Ahmad <imuhammadahmadazam@gmail.com>
2025-06-16 23:10:53 +0500

    Edited frontend text

--- frontend/src/components/home_hero/hero.jsx
+++ frontend/src/components/home_hero/hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
 
     return(
         <section className="hero-section" id='hero'>
-            <h1>Welcome to leading health and wellness retailer.</h1>
+            <h1>Welcome to leading health and wellness retailer. </h1>
             <h3>We believe in pure, natural goodness - plain and simple. We aim to partner with you on your unique wellness journey while honoring people and the planet in all that we do.</h3>
             <div>
                 <Link to='/products/All'><button>Shop now</button></Link>
